c4f2ab4363dc3384698b4fd78564cb7589a7f06c
to0ken <evgeniamafinova@gmail.com>
2026-04-29 08:48:10 +0300

    Create database.js

---
+++ auth_service/database/database.js
@@ -0,0 +1,22 @@
+import pg from 'pg';
+const { Pool } = pg
+
+
+let pool;
+
+export async function initDatabase() {
+    pool = await new Pool({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } })
+    await pool.query(`
+        CREATE TABLE users(
+            id SERIAL PRIMARY KEY,
+            login TEXT UNIQUE NOT HULL,
+            password TEXT NOT NULL,
+            created_at CURRENT_TIMESTAMP,
+        )
+        `)
+    return pool;
+}
+
+export async function getDatabase() {
+    return pool;
+}
