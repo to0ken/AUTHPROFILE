412f60c7ff4fd13a560790f1874b90f7ac1100c3
to0ken <evgeniamafinova@gmail.com>
2026-04-29 10:40:38 +0300

    Update api.js

--- auth_service/routes/api.js
+++ auth_service/routes/api.js
@@ -1,32 +1,51 @@
 import express from 'express'
+import { getDatabase } from '../database/database.js';
+import bcrypt from "bcrypt"
 
 const router = express.Router();
 
 
-router.post('/login', (req, res) =>{
-    const{login, password} = req.body??{}
+router.post('/login', async(req, res) => {
+    const { login, password } = req.body ?? {}
+
+    const db = await getDatabase();
+    const user = await db.query(
+        `
+        SELECT * FROM users WHERE login=$1
+        `, [login])
+    const isPasswordvalid = await bcrypt.compare(password, user)
 
 
-    if(!login || !password && (!password && !login)) {
-        res.status(401).send({status:"error"})
+    if (login !== user.login) {
+        res.status(401).send({ status: "error" })
     }
-    if(login === "test" && password === "test"){
-        res.status(200).send({status: "seccess"})
+    if (!login || !password) {
+        res.status(401).send({ status: "error" })
     }
-    else{
-        res.status(401).send({status:"error"})
+    if (isPasswordvalid) {
+        res.status(200).send({ status: "success" })
+    }
+    else {
+        res.status(501).send({ status: "error" })
     }
 })
 
-router.post("/register", (req, res) => {
-  const { login, password } = req.body ?? {};
+router.post("/register", async (req, res) => {
+    const { login, password } = req.body ?? {};
 
-  const db = getDatabase();
-  db.run(`
-    INSERT INTO users (login, password) VALUE ('${login}', '${password}')
-  `)
-  res.status(200).send({ status: "seccess" });
+    const db = getDatabase();
+    const hashPassword = await bcrypt.hash(password, 12)
+    if (password.lenght <= 4){
+        res.status(401).send({status:"error"})
+    }else{
+    db.query(`
+    INSERT INTO users (login, password)
+     VALUE ('$1', '$2')
+  `, [login, hashPassword])
+    res.status(200).send({ status: "seccess" });
+    }
 
 });
+
 
 export default router;
