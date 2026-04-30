5160ebd15a5f7e33ee311d0d2e2d61b644bc887f
to0ken <evgeniamafinova@gmail.com>
2026-04-30 12:57:49 +0300

    Update server.js

--- auth_service/server.js
+++ auth_service/server.js
@@ -1,29 +1,25 @@
-import express from 'express'
+import express from 'express';
 import 'dotenv/config'
-import {initDatabase} from "./database/database.js"
+import {initDatabase} from "./database/database.js";
+import apiRoutes from "./routes/api.js";
+import pageRoutes from "./routes/pages.js";
+import {fileURLToPath} from "url";
+import path from "path";
 
-import apiRoutes from "./routes/api.js"
-import pageRoutes from "./routes/pages.js"
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename)
-
-const app = express()
+const app = express();
 const port = process.env.AUTH_PORT || 3000;
 
 await initDatabase();
 
-
-app.use(express.static('public'));
-app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use("/api",apiRoutes)
-app.use("/",pageRoutes)
-
+app.use("/", pageRoutes);
+app.use("/api", apiRoutes);
 
 app.listen(port, () => {
-  console.log(`Server is running on ${port}`);
-})
-
-// для создания бс - npm install sqlite sqlite3 bcrypt
+    console.log(`Server started http://localhost:${port}`);
+});
