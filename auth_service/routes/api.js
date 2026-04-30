d3ed7f7d1e8bd058a8c34be00467ed4a1e709c9b
to0ken <evgeniamafinova@gmail.com>
2026-04-30 12:57:07 +0300

    Update api.js

--- auth_service/routes/api.js
+++ auth_service/routes/api.js
@@ -1,51 +1,50 @@
-import express from 'express'
+import express from 'express';
 import { getDatabase } from '../database/database.js';
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
 
 const router = express.Router();
 
-
-router.post('/login', async(req, res) => {
-    const { login, password } = req.body ?? {}
-
-    const db = await getDatabase();
-    const user = await db.query(
-        `
-        SELECT * FROM users WHERE login=$1
-        `, [login])
-    const isPasswordvalid = await bcrypt.compare(password, user)
-
-
-    if (login !== user.login) {
-        res.status(401).send({ status: "error" })
-    }
-    if (!login || !password) {
-        res.status(401).send({ status: "error" })
-    }
-    if (isPasswordvalid) {
-        res.status(200).send({ status: "success" })
-    }
-    else {
-        res.status(501).send({ status: "error" })
-    }
-})
-
-router.post("/register", async (req, res) => {
+router.post('/login', async (req, res) => {
     const { login, password } = req.body ?? {};
 
-    const db = getDatabase();
-    const hashPassword = await bcrypt.hash(password, 12)
-    if (password.lenght <= 4){
-        res.status(401).send({status:"error"})
-    }else{
-    db.query(`
-    INSERT INTO users (login, password)
-     VALUE ('$1', '$2')
-  `, [login, hashPassword])
-    res.status(200).send({ status: "seccess" });
+    const db = await getDatabase();
+    const result = await db.query('SELECT * FROM users WHERE login=$1', [login]);
+    const user = result.rows[0]; // id, login, password, ctrated_at
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (login !== user.login) {
+        res.status(401).send({ status: "error" });
+    }
+    if (!login || !password) {
+        res.status(401).send({ status: "error" });
+    }
+    if (isPasswordValid) {
+        res.status(200).send({
+            status: "success",
+            user_id: user.id,
+            user_name: user.login,
+            user_password: user.password,
+            user_add: user.created_at
+        });
+    }
+    else {
+        res.status(501).send({ status: "error" });
     }
 
 });
 
+router.post('/register', async (req, res) => {
+
+    const { login, password } = req.body ?? {};
+    const db = await getDatabase();
+    const hashPassword = await bcrypt.hash(password, 12);
+    if (password.length <= 4 || !login || !password) {
+        res.status(401).send({ status: "error" });
+    }
+    else {
+        db.query('INSERT INTO users (login, password) VALUES ($1, $2)', [login, hashPassword]);
+        res.status(200).send({ status: "success" });
+    }
+
+});
 
 export default router;
